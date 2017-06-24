3258159120615a0554d5491c9a1980097a2f3b02
nikulanton <nikulanton1997@gmail.com>
2017-06-24 14:54:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,17 +11,5 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(commands=['reg'])
 # Регистрируем пользователя при первом входе
 def first_visit(message):
-    # В дальнейшем одинаковая строка коннекта к БД
-    conn = psycopg2.connect(host="ec2-107-22-162-158.compute-1.amazonaws.com",
-                            user="jwmxuqiprgblvo",
-                            password="3f0358afe49052171dd76a4166bab6709341705b3a5223b497f48066cb6f85e4",
-                            dbname="da5f2k12t4ep54",
-                            port=5432)
-    curs = conn.cursor()
-    # Создаем курсор, выполняем запрос на выборку id зарегистрированных пользователей
-    curs.execute('SELECT test FROM mytable;')
-    ids = curs.fetchall()
-    bot.send_message(message.chat.id, ids[0][0])
+    bot.send_message(message.chat.id, 'Приветики')
     conn.close()
-
-bot.polling()
