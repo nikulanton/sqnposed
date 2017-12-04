b6c76780e336628785561231c2c6a9918c6157e1
nikul <nikulanton1997@gmail.com>
2017-12-04 18:54:09 +0300

    ...

--- bot.py
+++ bot.py
@@ -49,10 +49,12 @@ def user_register(message):
         bot.send_message(message.chat.id, 'Вы уже зарегистрированы в системе!')
     else:
         # Если нет, добавляем в базу
-        curs.execute('INSERT INTO users (user_id,nickname,team,role,usermode) VALUES (%s,%s,NULL,user,NULL);',
-                     (message.chat.id, message.from_user.username))
+        userrole = 'user'
+        curs.execute('INSERT INTO users (user_id,nickname,team,role,usermode) VALUES (%s,%s,NULL,%s,NULL);',
+                     (message.chat.id, message.from_user.username, userrole))
         bdconnect.commit()
         bot.send_message(message.chat.id, 'Вы успешно зарегистрированы')
+    bdconnect.close()
 
 @bot.message_handler(commands=['list'])
 def list_of_quests(message):
@@ -63,23 +65,27 @@ def list_of_quests(message):
     for quest in quests:
         allquests = allquests + str(quest[0]) + '\n'
     bdconnect.commit()
+    bdconnect.close()
     bot.send_message(message.chat.id, allquests)
 
 @bot.message_handler(commands=['addquest'])
 def list_of_quests(message):
-    list_cursor = bdconnect.cursor()
-    list_cursor.execute('SELECT quest_title FROM quests;')
-    quests = list_cursor.fetchall()
-    allquests = "Список доступных квестов:\n"
-    for quest in quests:
-        allquests = allquests + str(quest[0]) + '\n'
+    add_cursor = bdconnect.cursor()
+    add_cursor.execute('UPDATE users set usermode="addquest" WHERE user_id = %s;', (int(message.chat.id),))
     bdconnect.commit()
-    bot.send_message(message.chat.id, allquests)
+    bdconnect.close()
+    bot.send_message(message.chat.id, 'Введите данные о квесте в формате название;количество опыта;количество денег;дата начала;дата окончания\n*Дата вводится в формате день/месяц/год')
 
 
 @bot.message_handler(content_types=['text'])
 def some_text_reaction(message):
-    bot.send_message(message.chat.id, 'К сожалению, мы еще не умеем отвечать на то что ты отправил')
+    get_usermode = bdconnect.cursor()
+    get_usermode.execute('SELECT  usermode FROM users WHERE user_id=%s', (int(message.chat.id),))
+    usermode = get_usermode.fetchall()
+    if usermode[0][0] == 'addquest':
+        bot.send_message(message.chat.id, 'Вы в режиме добавления квеста')
+    else:
+        bot.send_message(message.chat.id, 'Тут ничего нет :(')
 
 @server.route('/bot', methods=['POST'])
 def getMessage():
