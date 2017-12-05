701411d9202cd33348a0b57341c6d25ea039003e
nikul <nikulanton1997@gmail.com>
2017-12-05 23:52:13 +0300

    ...

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ def list_of_quests(message):
     quests = curs.fetchall()
     allquests = "Список доступных квестов:\n"
     for quest in quests:
-        allquests = allquests + 'Квест номер {0} - {1}\nОпыт: {2}, Монеты: {3}\nЛокация: {4}'.format(quest[0],quest[1],quest[2],quest[3],quest[5])
+        allquests = allquests + 'Квест номер {0} - {1}\nОпыт: {2}, Монеты: {3}\nЛокация: {4}\n'.format(quest[0],quest[1],quest[2],quest[3],quest[5])
     bdconnect.commit()
     bot.send_message(message.chat.id, allquests)
 
@@ -115,7 +115,7 @@ def answer(message):
 @bot.message_handler(commands=['stats'])
 def stats(message):
     curs = bdconnect.cursor()
-    curs.execute('WITH vse AS (SELECT * FROM quest_progress JOIN quests ON (quest_progress.quest_id=quests.quest_id)), nado AS (SELECT quest_exp,quest_money FROM vse WHERE user_id=%s) SELECT sum(quest_exp),sum(quest_money) FROM nado', (int(message.chat.id),))
+    curs.execute('WITH vse AS (SELECT * FROM quest_progress JOIN quests ON (quest_progress.quest_id=quests.quest_id)), nado AS (SELECT user_id,quest_exp,quest_money,locations.loc_exp,isdoing FROM vse JOIN locations ON (quest_location=loc_id)) SELECT sum(quest_exp),sum(quest_money),sum(loc_exp) FROM nado WHERE user_id=%s AND isdoing=TRUE', (int(message.chat.id),))
     exp = curs.fetchall()
     bot.send_message(message.chat.id, 'Количество опыта: {0}\nКоличество монет: {1}'.format(exp[0][0],exp[0][1]))
 
