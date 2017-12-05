2a04dba50f0697105f27fb98a4e72a01b285fba4
nikul <nikulanton1997@gmail.com>
2017-12-05 17:12:25 +0300

    ...

--- bot.py
+++ bot.py
@@ -124,33 +124,36 @@ def some_text_reaction(message):
             bot.send_message(message.chat.id, 'Вы успешно взяли квест. Отправляем первое задание...')
             bot.send_message(message.chat.id, first_task[0][1])
     elif usermode[0][0] == 'tellinganswer':
-        textcursor.execute('SELECT current_task,quest_id FROM quest_progress WHERE user_id=%s', (int(message.chat.id),))
+        textcursor.execute('SELECT current_task,quest_id FROM quest_progress WHERE user_id=%s AND isdoing=FALSE', (int(message.chat.id),))
         current_task_id = textcursor.fetchall()
-        textcursor.execute('SELECT task_answer FROM tasks WHERE task_id=%s AND task_quest=%s',
-                     (current_task_id[0][0], current_task_id[0][1],))
-        istrueanswer = textcursor.fetchall()
-        if istrueanswer[0][0] == message.text:
-            bot.send_message(message.chat.id,current_task_id[0][0]+current_task_id[0][1])
-            textcursor.execute('UPDATE task_progress SET isdoing=TRUE WHERE task_id=%s AND user_id=%s AND quest_id=%s',
-                         (int(current_task_id[0][0]),int(message.chat.id), int(current_task_id[0][1]),))
-            bdconnect.commit()
-            bot.send_message(message.chat.id, 'Задание успешно выполнено, поздравляем! Отправляем следующее...')
-            textcursor.execute('SELECT max(task_id) FROM tasks WHERE task_quest=%s', (current_task_id[0][1],))
-            max_taskid = textcursor.fetchall()
-            bot.send_message(message.chat.id, max_taskid[0])
-            if max_taskid[0]==current_task_id[0][0]:
-                textcursor.execute('UPDATE quest_progress SET isdoing=TRUE WHERE user_id=%s AND quest_id=%s',(int(message.chat.id),current_task_id[0][1],))
-                bot.send_message(message.chat.id,'УПС! Заданий больше не осталось, похоже вы выполнили квест!')
-            else:
-                textcursor.execute('UPDATE quest_progress SET current_task=%s WHERE quest_id=%s AND user_id=%s',
-                                   (current_task_id[0][0]+1,current_task_id[0][1],int(message.chat.id),))
-                textcursor.execute('SELECT task_id,task_text,task_title FROM tasks WHERE task_quest=%s AND task_id=%s ORDER BY task_id',
-                    (current_task_id[0][1],current_task_id[0][0]+1,))
-                bdconnect.commit()
-                next_task = textcursor.fetchall()
-                bot.send_message(message.chat.id, next_task[0][1])
+        if not current_task_id:
+            bot.send_message(message.chat.id, 'Сначала возьмите на выполнение квест')
         else:
-            bot.send_message(message.chat.id, 'Ответ не верный! Попробуйте другой!')
+            textcursor.execute('SELECT task_answer FROM tasks WHERE task_id=%s AND task_quest=%s',
+                     (current_task_id[0][0], current_task_id[0][1],))
+            istrueanswer = textcursor.fetchall()
+            if istrueanswer[0][0] == message.text:
+                bot.send_message(message.chat.id,current_task_id[0][0]+current_task_id[0][1])
+                textcursor.execute('UPDATE task_progress SET isdoing=TRUE WHERE task_id=%s AND user_id=%s AND quest_id=%s',
+                         (int(current_task_id[0][0]),int(message.chat.id), int(current_task_id[0][1]),))
+                bdconnect.commit()
+                bot.send_message(message.chat.id, 'Задание успешно выполнено, поздравляем! Отправляем следующее...')
+                textcursor.execute('SELECT max(task_id) FROM tasks WHERE task_quest=%s', (current_task_id[0][1],))
+                max_taskid = textcursor.fetchall()
+                bot.send_message(message.chat.id, max_taskid[0])
+                if max_taskid[0]==current_task_id[0][0]:
+                    textcursor.execute('UPDATE quest_progress SET isdoing=TRUE WHERE user_id=%s AND quest_id=%s',(int(message.chat.id),current_task_id[0][1],))
+                    bot.send_message(message.chat.id,'УПС! Заданий больше не осталось, похоже вы выполнили квест!')
+                else:
+                    textcursor.execute('UPDATE quest_progress SET current_task=%s WHERE quest_id=%s AND user_id=%s',
+                                   (current_task_id[0][0]+1,current_task_id[0][1],int(message.chat.id),))
+                    textcursor.execute('SELECT task_id,task_text,task_title FROM tasks WHERE task_quest=%s AND task_id=%s ORDER BY task_id',
+                    (current_task_id[0][1],current_task_id[0][0]+1,))
+                    bdconnect.commit()
+                    next_task = textcursor.fetchall()
+                    bot.send_message(message.chat.id, next_task[0][1])
+            else:
+                bot.send_message(message.chat.id, 'Ответ не верный! Попробуйте другой!')
     else:
         bot.send_message(message.chat.id, 'Тут ничего неет :(')
 
